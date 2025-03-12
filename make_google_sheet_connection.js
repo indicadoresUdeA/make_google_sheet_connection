f0cf1faca6fc35a8600e98702a6f4a29e0cd0bf9
root <root@srv648005.hstgr.cloud>
2025-03-12 19:20:57 +0000

    first commit

---
+++ make_google_sheet_connection.js
@@ -0,0 +1,24 @@
+function fetchDataFromAPI_New() {
+  // URL del nuevo webhook
+  var url = "https://hook.us1.make.com/t6hvvlt3r711uja555wmrk063dhabr7u";
+  var response = UrlFetchApp.fetch(url);
+  var data = JSON.parse(response.getContentText());
+  
+  // Obtener la hoja activa y limpiar el contenido previo
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  sheet.clear();
+  
+  // Obtener los encabezados a partir de las claves del primer objeto
+  var headers = Object.keys(data[0]);
+  sheet.appendRow(headers);
+  
+  // Recorrer cada objeto y agregar los datos a la hoja
+  data.forEach(function(item) {
+    var row = [];
+    headers.forEach(function(header) {
+      // Se agrega el valor si existe o se deja vacío en caso de null
+      row.push(item[header] !== null ? item[header] : "");
+    });
+    sheet.appendRow(row);
+  });
+}
